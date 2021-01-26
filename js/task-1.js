0b46e68b606878af1690184e98d9ff3164780fdb
dmytman <68687065+dmytman@users.noreply.github.com>
2021-01-26 21:55:27 +0200

    Add files via upload

---
+++ js/task-1.js
@@ -0,0 +1,44 @@
+const str = 'Привіт';
+console.log(typeof str);
+
+const num = 123;
+console.log(typeof num);
+
+const flag = true;
+console.log(typeof flag);
+
+const txt = true;
+console.log(typeof txt);
+
+//////////////////////////
+
+const age = 49;
+console.log('age -',age);
+
+const name = 'dmytro';
+console.log('name -',name);
+
+const surname = 'mysikhin';
+console.log('surname -',surname);
+
+const number = 13;
+console.log('number in hostal -',number);
+
+const figure = 'circle';
+console.log(figure);
+
+
+let a = 16;
+console.log(a);
+
+let b = 0;
+console.log(b);
+
+let work = 'flirance';
+console.log('My work is:',work);
+
+let year = 2021;
+console.log('year -',year);
+
+let zodiac = 'fish';
+console.log('zodiac -' ,zodiac);
