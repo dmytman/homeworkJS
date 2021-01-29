58016cf86f6a7f974f1e31cc01dd7c0186c4892b
dmytman <dmytman@gmail.com>
2021-01-29 12:28:43 +0200

    completed task-1

---
+++ module-1/practiceWork-1/js/task-1.js
@@ -0,0 +1,16 @@
+console.log('task-1');
+
+const str = 'Привіт';
+console.log(typeof str);
+
+const num = 123;
+console.log(typeof num);
+
+const flag = true;
+console.log(typeof flag);
+
+const txt = 'true';
+console.log(typeof txt);
+
+//////////////////////////
+
---
+++ module-1/practiceWork-1/js/task-2.js
@@ -0,0 +1,35 @@
+console.log('task-2');
+
+
+console.log('const :');
+const yourAge = 49;
+console.log('age -',yourAge);
+
+const yourName = 'dmytro';
+console.log('name -',yourName);
+
+const surName = 'mysikhin';
+console.log('surname -',surName);
+
+const userName = 'dmytman';
+console.log('nickname -',userName);
+
+const userEmail = 'antonio@gmail.com';
+console.log(userEmail);
+
+
+console.log('let :');
+let youEat = false;
+console.log('are you eating now:',youEat);
+
+let haveDog = false;
+console.log('you have a dog:',haveDog);
+
+let work = 'flirance';
+console.log('My work is:',work);
+
+let haveCat = 0;
+console.log('have many cats do you have: ',haveCat);
+
+let zodiac;
+console.log('your zodiac: ',zodiac);
