497906c22ea5431051f58b8c11574eabaa46e60f
dmytman <dmytman@gmail.com>
2021-02-15 16:59:20 +0200

    corrected mistakes

--- module-1/finalTask-1/js/task-4.js
+++ module-1/finalTask-1/js/task-4.js
@@ -5,17 +5,21 @@ const pricePerDroid = 3000;
 const queDroid = prompt('Скільки ви хочете купити дроїдів?');
 let totalPrice = queDroid * pricePerDroid;
 
+while(true) {
 if (queDroid === null) {
     console.log('Скасовано користувачем!');
-} else {
-    console.log(totalPrice);
+break
+} 
+if  (totalPrice > credits) {
+    alert('Недостатнто коштів на рахунку!')
+    break
 }
-if (totalPrice > credits) {
-    console.log('Недостатнто коштів на рахунку!')
-} else {
+else{
     const remainder = credits - totalPrice;
     console.log(remainder);
     alert(
       `Ви купили ${queDroid} дроїдів, на рахунку залишилося ${remainder} кредитів.`
     );
+    break
+} 
 }
