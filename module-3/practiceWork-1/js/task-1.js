e1c7836fd6d733046aebf6da762fa8e69eaa49f9
dmytman <dmytman@gmail.com>
2021-04-05 22:07:13 +0300

    correct an error in module-3 task1 and module-4 task4

--- module-3/practiceWork-1/js/task-1.js
+++ module-3/practiceWork-1/js/task-1.js
@@ -1,15 +1,14 @@
-console.log('task-1.js');
+console.log("task-1.js");
 
 const user = {};
 
 user.userName = "dmytro";
 user.age = 45;
 user.showUserName = function () {
-    return this.userName;
+  return this.userName;
 };
 user.updateAge = function () {
-    const a = this.age += 1;  
-    return a;
+  return (this.age += 1);
 };
 
 console.log(user.updateAge());
